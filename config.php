dee35378283642ca503b28f8c9b7f46b57925a78
Oscar Reyes <oscar.reyes@licklider.cl>
2023-04-08 01:29:08 -0400

    cambio a .gitignore +1

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'user');
-    define('DB_PASSWORD', 'pass');
+    define('DB_PASSWORD', 'pasjjjjs');
     define('DB_NAME', 'db');
 ?>
